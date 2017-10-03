fb0ccd291c66d17db7f574d1c339ccce01fa4af7
Aaron Whitehead <Aaronbmwhitehead@gmail.com>
2017-10-02 22:47:38 -0700

    Updated to use new formatting. Updated to be python3 compatible.

--- update.py
+++ update.py
@@ -1,45 +1,27 @@
 import pygsheets
 import myfitnesspal
+import config
 from datetime import date, timedelta
 
 gc = pygsheets.authorize()
-print "Google Sheets Auth sucessful"
-client = myfitnesspal.Client('hellbound911@live.com')
-print "MFP Auth sucessful"
+print("Google Sheets Auth successful")
+client = myfitnesspal.Client(config.username, config.password)
+print("MFP Auth successful")
 
-originDate = date(2017, 1, 17)
+originDate = date(2017, 9, 30)
 today = date.today()
 
 dayOffset = abs(today - originDate)
-print "day offset: " + str(dayOffset.days)
+print("day offset: " + str(dayOffset.days))
 
-#grab mfp data for yesterday
-yesterday = date.today() - timedelta(days=1)
-mfpData = client.get_date(yesterday.year, yesterday.month, yesterday.day)
+mfpData = client.get_date(today.year, today.month, today.day)
 weight = client.get_measurements('Weight')
  
-#update the cells
+# update the cells
 wks = gc.open_by_key('1Ld1TnVGJUkJ3VJHTvN6jlgi5-DGQJbnuxYJk3j1hZWs')
 sh = wks[0]
-sh.update_cell("B"+str(1+dayOffset.days), weight.items()[0][1])
-sh.update_cell("C"+str(1+dayOffset.days), mfpData.totals['carbohydrates'])
-sh.update_cell("D"+str(1+dayOffset.days), mfpData.totals['fat'])
-sh.update_cell("E"+str(1+dayOffset.days), mfpData.totals['protein'])
-
-#Calculate streak on keto
-test = sh.cell((1+dayOffset.days,3))
-streak = 0
-it = 0
-print(test.value)
-print(1+dayOffset.days-it)
-print((test.value <=20) and (1+dayOffset.days-it >1))
-print(int(test.value) <=20)
-print(1+dayOffset.days-it >1)
-while((int(test.value) <=20) and (1+dayOffset.days-it >1)):
-    print(test.value)
-    streak += 1
-    it += 1
-    test = sh.cell((1+dayOffset.days-it,3))
-
-sh.update_cell((55,12), streak)
+sh.update_cell("B"+str(dayOffset.days), list(weight.items())[0][1])
+sh.update_cell("C"+str(dayOffset.days), mfpData.totals.get('carbohydrates', 0))
+sh.update_cell("D"+str(dayOffset.days), mfpData.totals.get('fat', 0))
+sh.update_cell("E"+str(dayOffset.days), mfpData.totals.get('protein', 0))
 
