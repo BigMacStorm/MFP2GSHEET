009eaa6c6d3a8f282e47380826e4600bc672849e
Aaron Whitehead <aaronbmwhitehead@gmail.com>
2018-05-15 17:57:47 -0700

    Modify the updater to add rolling date to make the graph look a little better

--- update.py
+++ update.py
@@ -17,6 +17,7 @@ weight = client.get_measurements('Weight')
 # update the cells
 wks = gc.open_by_key('1Ld1TnVGJUkJ3VJHTvN6jlgi5-DGQJbnuxYJk3j1hZWs')
 sh = wks[0]
+sh.update_cell("A"+str(dayOffset.days+1), "=A"+str(dayOffset.days)+"+1")
 sh.update_cell("B"+str(dayOffset.days+1), list(weight.items())[0][1])
 sh.update_cell("C"+str(dayOffset.days+1), mfpData.totals.get('carbohydrates', 0))
 sh.update_cell("D"+str(dayOffset.days+1), mfpData.totals.get('fat', 0))
